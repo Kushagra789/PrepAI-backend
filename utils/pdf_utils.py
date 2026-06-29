a148f8c764b6017c8fcd818b630403a237cb03c6
Kushagra Pandey <kushagra.kp08@gmail.com>
2026-06-29 10:49:32 +0530

    Fix Render deployment

--- utils/pdf_utils.py
+++ utils/pdf_utils.py
@@ -2,7 +2,13 @@ import fitz
 import pytesseract
 from PIL import Image
 import io
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+import os
+
+# Set Windows path only when running on Windows
+if os.name == "nt":
+    pytesseract.pytesseract.tesseract_cmd = (
+        r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+    )
 
 
 def extract_text_from_pdf(pdf_path):
@@ -13,19 +19,18 @@ def extract_text_from_pdf(pdf_path):
     for page in document:
         page_text = page.get_text()
 
-        # If PDF already has text
+        # PDF already contains selectable text
         if page_text.strip():
             text += page_text
 
-        # If PDF is scanned, use OCR
+        # Scanned PDF → OCR
         else:
-            pix = page.get_pixmap()
-
-            image = Image.open(io.BytesIO(pix.tobytes("png")))
-
-            ocr_text = pytesseract.image_to_string(image)
-
-            text += ocr_text
+            try:
+                pix = page.get_pixmap()
+                image = Image.open(io.BytesIO(pix.tobytes("png")))
+                text += pytesseract.image_to_string(image)
+            except Exception:
+                pass
 
     document.close()
 
